fe2d8adecb5c2be86e36ccfe4f2f2a2afac9fad7
João Pedro Reis Felini <137925017+Ja1Rf@users.noreply.github.com>
2023-10-15 00:31:07 -0300

    Adicionei saveTask para salvar no localstorage(JS)

--- js/index.js
+++ js/index.js
@@ -1,46 +1,65 @@
+
+let taskId = 1;
+
+function add_task() {
+    const titulo = document.getElementById('input_1').value;
+    const descricao = document.getElementById('area_descricao').value;
+    if (!titulo){
+        return false;
+    }else{
+        salvarEssesDados(titulo, descricao);
+    }
+}
+
+function limpacampos() {
+    document.getElementById('input_1').value = '';
+    document.getElementById('area_descricao').value = '';
+}
+
+function carregarDados(title, descricao, id) {
+    document.getElementById('input_1').value = title;
+    document.getElementById('area_descricao').value = descricao;
+}
+
+function salvarEssesDados(title, descricao) {
+    var novaTarefa = document.createElement('div');
+    novaTarefa.innerHTML = `
+        <div class="card text-bg-success mb-3" style="max-width: 18rem; cursor: pointer;" onclick="carregarDados('${title}', `+'`'+`${descricao}`+'`'+`, ${taskId})" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            <div class="card-header d-flex justify-content-between">
+                <h5 class="card-title text-center" id="title_card_view_${taskId}">${title}</h5>
+                <i class="fa-solid fa-trash fa-xl" onclick="deletarItem(event)"></i>
+            </div>
+            <div class="card-body">
+                <p class="card-text" id="description_card_view_${taskId}">${descricao ? descricao : "Sem Descrição"}</p>
+            </div>
+        </div>
+    `;
+    // novaTarefa.innerHTML = `
+    // <div id="espaco_card">
+    // <div id="input_trash">
+    // <input placeholder="Título" id="task_card_${taskId}" value="${title}" style="${getComputedStyle(document.getElementById('input_1')).cssText}">
+    // <i class="fa-solid fa-trash fa-xl" onclick="deletarItem('${taskId}')"></i>
+    // </div>
+    // <textarea placeholder="A vingança nunca é plena, mata a alma e a envenena!" name="descricao" id="textearea_descricao" cols="30" rows="10" style="${getComputedStyle(document.getElementById('area_descricao')).cssText}">${descricao}</textarea>
+    // <p id="time"></p>
+    // </div>
+    // `;
+    
+    var novaColuna = document.getElementById('new_task');
+    novaColuna.appendChild(novaTarefa);
+    taskId++;
+}
+
+function deletarItem(event) {
+    event.target.parentElement.parentElement.remove();
+}
+
+
 function mudaFundo(event, cor) {
-    document.body.style.backgroundColor = cor 
+    document.body.style.backgroundColor = cor;
 }
 
 function background() {
     let div = document.getElementById('color_menu');
     div.style.display = div.style.display === 'block' ? 'none' : 'block';
-    
 }
-
-/*
-
-explicando o cófigo js:
-
-function mudaFundo(event, cor) {
-    document.body.style.backgroundColor = cor
-}
-
-///no HTML foi criado as cores (hexadecimais) e colocado como argumento um 'evento' 
-que vai mostrar no meu código qnd apertar num botao (cor do botao) mostra oq foi feito (click do botao).
-nessa função do mudaFUndo com os parâmetros ja chamados é adicionado um comando.
-Ao colocar document (ou seja, ele vai ler o documento HTML todo desde o DOCTYPE la em cima)
-e entrar no body, após isso estiliza a tela com backgroundColor(comando que irá mudar a cor da tela)
-que irá receber ('=') a cor (hexadecimais postos no meu código)
-
-function background() {
-    let div = document.getElementById('color_menu');
-    div.style.display = div.style.display === 'block' ? 'none' : 'block';
-    
-}
-
-/// nessa função ele vai ter interação com o botão, ou seja, vai ser aquele sistema em que qnd
-clicar no botão ele abre para baixo as cores e pode ser mudado o background ok.
-
-ali é criado um let (uma variavel sla é a mesma coisa) declarada(chamada) como div, onde vai receber os comandos
-em que entra no document (HTML) e procura o Id, o comando getElementById('color_menu'); vai pegar
-um id específico e poder mexer nele, onde entra o comando debaixo;
-
-div.style.display = div.style.display === 'block' ? 'none' : 'block';
-
-//nesse comando ele vai pegar a div e estilizar o display (bolinha colorida) onde se for 'block'
- ou seja, se torna vísivel (ele abre com as cores abaixo), se n for identico ao bloco ent vai ser 'none'
- , se for 'none' ele fecha o bloco, mantém somente a imagem da bolinha colorida  
-
-
-*/ 
